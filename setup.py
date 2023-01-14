b63813af6632395a0d916b2ca24758ad9e526cf7
EgorChalapko <60626022+EgorChalapko@users.noreply.github.com>
2023-01-14 17:28:50 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iris-ml-app",
+    version="0.0.1",
+    description="An app about color of flowers",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["streamlit", "pandas", "sklearn", "sklearn.ensemble"],
+    entry_points={"console_scripts": ["iris-ml-app = src.main:main"]},
+)
